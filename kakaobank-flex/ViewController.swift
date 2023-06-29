9dd5a03c9e61dccff6314c291f16a2d7439550ab
sooakim <alice2863@naver.com>
2023-06-29 23:25:37 +0900

    Initial Commit

---
+++ kakaobank-flex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kakaobank-flex
+//
+//  Created by 김수아 on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kakaobank-flexUITests/kakaobank_flexUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kakaobank_flexUITestsLaunchTests.swift
+//  kakaobank-flexUITests
+//
+//  Created by 김수아 on 2023/06/29.
+//
+
+import XCTest
+
+final class kakaobank_flexUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
